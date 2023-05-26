1104fb9006f05072adc60506096f49f9102c5891
Okuku Itorobong <patrickokuku1@gmail.com>
2023-05-26 14:17:40 +0100

    png images and ferris wheel animation fixes

--- src/components/FerrisWheel/FerrisWheel.jsx
+++ src/components/FerrisWheel/FerrisWheel.jsx
@@ -1,23 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "./FerrisWheelStyles.scss";
-import Polygon from "../Polygon/Polygon";
-import { polygonPositions } from "../../constants";
+// import Polygon from "../Polygon/Polygon";
+import { imgPositions } from "../../constants";
 
 const FerrisWheel = () => {
-  const [animated, setAnimated] = useState(false);
+  const [init, setInit] = useState(false);
+  const [initAngle, setInitAngle] = useState(0);
+  const [wheelAngle, setWheelAngle] = useState(0);
 
   const handleClick = () => {
-    setAnimated(true);
+    setInitAngle(wheelAngle + 10);
+    setWheelAngle((prevAngle) => prevAngle - 90);
 
+    setInit(true);
     setTimeout(() => {
-      setAnimated(false);
-    }, 1500);
+      setInit(false);
+    }, 400);
   };
+
   return (
     <div className="wheel-container">
-      <div className={`wheel ${animated ? "animate-wheel" : ""}`}>
-        {polygonPositions.map((position, i) => (
-          <Polygon key={i} onClick={handleClick} position={position} />
+      <div
+        style={{
+          transform: `rotate(${init ? initAngle : wheelAngle}deg) scale(2.3) `,
+          transition: "all  1s ease-out",
+        }}
+        className="wheel"
+      >
+        {imgPositions.map((position, i) => (
+          <img
+            key={i}
+            src={position.img}
+            style={{
+              top: `${position.top}rem`,
+              left: `${position.left}rem`,
+              height: `${position.height}px`,
+              width: `${position.width}px`,
+            }}
+            onClick={handleClick}
+          />
         ))}
       </div>
     </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,23 +1,31 @@
 /* Polygons Position Data */
-export const polygonPositions = [
+export const imgPositions = [
   {
-    angle: 90,
-    top: 4,
-    left: -7.9,
+    top: 3.5,
+    left: -7,
+    height: 170,
+    width: 150,
+    img: "/img/hero1.png",
   },
   {
-    angle: 0,
-    top: -6.7,
-    left: 4.6,
+    top: -6.9,
+    left: 3.6,
+    height: 150,
+    width: 170,
+    img: "/img/hero2.png",
   },
   {
-    angle: 90,
     top: 4,
     left: 15.6,
+    height: 170,
+    width: 150,
+    img: "/img/hero3.png",
   },
   {
-    angle: 0,
-    top: 16.6,
-    left: 4.6,
+    top: 15.3,
+    left: 3.6,
+    height: 150,
+    width: 170,
+    img: "/img/hero4.png",
   },
 ];
